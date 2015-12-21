6389466230a2898fb45576bd37fbee6555414093
Michał Pacek <m.pacek@onet.eu>
2015-12-21 12:10:04 +0100

    Stickyitem ver. 1.0.0

---
+++ README.md
@@ -0,0 +1,21 @@
+#Stickyitem v.1.0.0
+
+jQuery helper plugin, adds class to elements when window is scrolled to it's position.
+
+**Requirements:**
+
+* jQuery
+
+**How to use:**
+
+``` JavaScript
+if ($.fn.stickyitemSetup) {
+  $('.js-scrollto').stickyitemSetup({
+    stickyClass: 'is-sticky'
+	});
+}
+```
+
+Copyright 2015 Michał Pacek
+
+Licensed under the Apache License, Version 2.0.
---
+++ stickyitem.js
@@ -0,0 +1,60 @@
+"use strict";
+
+(function ($) {
+
+$.fn.stickyitemSetup = function (settings) {
+  settings = jQuery.extend({
+      stickyClass: 'is-sticky',
+      startPosition: 0 // add class earier, or later, eg. 100
+  }, settings);
+
+  var $stickyitem = $(this),
+      stickyitemPosTop = $stickyitem.offset().top + startPosition,
+      windowPosTop,
+      isSticked = false;
+
+  var stickItem = function () {
+    windowPosTop = $(window).scrollTop();
+
+    if (windowPosTop >= stickyitemPosTop) {
+      if (!isSticked) {
+        $stickyitem.addClass(settings.stickyClass);
+        isSticked = true;
+      }
+    } else {
+      if (isSticked) {
+        $stickyitem.removeClass(settings.stickyClass);
+        isSticked = false;
+      }
+    }
+  }
+
+  var runStickyitem = function () {
+    stickItem();
+
+    // check on scroll
+    $(window).on('scroll.stickyitem', function () {
+      stickItem();
+    });
+  }
+
+  var clearStickyitem = function () {
+    $stickyitem.removeClass(settings.stickyClass);
+    $(window).off('scroll.stickyitem');
+  }
+
+  // run on load
+  runStickyitem();
+
+  // Uses responsive.js helper plugin
+  // 'off' when on mobile
+  // 'on' when on desktop
+  if ($.fn.responsiveSetup) {
+    $(document).responsiveSetup({
+      onMobile: { callFunction: clearStickyitem },
+      onDesktop: { callFunction: runStickyitem }
+    });
+  }
+}
+
+}(jQuery));
